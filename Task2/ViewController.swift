cf6eec1aa6106cd0bf81ef01ed068c4a059d1027
armaghan84 <armaghan.0084@gmail.com>
2021-04-22 05:08:27 +0500

    Initial Commit

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Armaghan  on 4/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
